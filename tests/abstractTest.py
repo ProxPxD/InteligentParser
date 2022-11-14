bce51ae6cb814ddc29e44c551a0200d2114eff8d
proxpxd <piotr10tutek@poczta.onet.pl>
2022-11-14 19:54:24 +0100

    add statistics and printing collective results in tests

--- tests/abstractTest.py
+++ tests/abstractTest.py
@@ -1,5 +1,6 @@
 import abc
 import unittest
+from typing import Iterable
 
 from smartcli.cli import Cli
 from smartcli.nodes.node import Root
@@ -10,6 +11,10 @@ class AbstractTest(unittest.TestCase, abc.ABC):
     cli: Cli | None = None
     root: Root | None = None
     half_sep_length = 40
+
+    total = 0
+    failure = 0
+    errors = 0
 
     @classmethod
     def print_sep_with_text(cls, text: str, sep: str = '*') -> None:
@@ -25,6 +30,24 @@ class AbstractTest(unittest.TestCase, abc.ABC):
     @classmethod
     def tearDownClass(cls) -> None:
         cls.root = None
+        cls.print_statistics(percentage=False)
+
+    @classmethod
+    def print_statistics(cls, failure=None, errors=None, total=None, short=False, percentage=True):
+        if failure is None:
+            failure = cls.failure
+        if errors is None:
+            errors = cls.errors
+        if total is None:
+            total = cls.total
+        failed = failure + errors
+        passed = total - failed
+        if short:
+            print(f'({failure}F, {errors}E, {passed}P)/{total}')
+        else:
+            print(f'Failed: {failed} (Failures: {failure}, Errors: {errors}), Passed: {passed}, Total: {total}')
+        if percentage:
+            print(f'Failed: {100*failed/total:.1f}% (Failures: {100*failure/total:.1f}%, Errors: {100*errors/total:.1f})%, Passed: {100*passed/total:.1f}%')
 
     def setUp(self) -> None:
         super().setUp()
@@ -42,12 +65,32 @@ class AbstractTest(unittest.TestCase, abc.ABC):
         is_failure = any(test == self for test, text in result.failures)
         passed = not (is_error or is_failure)
 
-        print('passed' if passed else 'ERROR' if is_error else 'FAIL' if is_failure else
+        self.__class__.total += 1
+        if is_error:
+            self.__class__.errors += 1
+        if is_failure:
+            self.__class__.failure += 1
+
+        print('PASS' if passed else 'ERROR' if is_error else 'FAIL' if is_failure else
             'WRONG UNIT TEST OUTCOME CHECKING! Investigate (possible incompatible with a python newer than 3.10)')
 
     @classmethod
     def _get_test_name(cls) -> str:
         return cls.__name__.removesuffix('Test')
+
+    def get_parameterized_methods_of_current_test(self, *method_nums) -> Iterable:
+        if not method_nums:
+            return iter([])
+
+        method_prefix = self.get_method_name() + '_'
+        child_methods = [name for name in dir(self) if name.startswith(method_prefix)]
+
+        if method_nums[0] is None:
+            method_nums = range(len(child_methods))
+
+        method_prefixes = (f'{method_prefix}{i}' for i in method_nums)
+        methods = (getattr(self, actual_name) for expected_prefix in method_prefixes for actual_name in child_methods if actual_name.startswith(expected_prefix))
+        return methods
 
     def run_current_test_with_params(self, *method_nums):
         '''
@@ -55,17 +98,5 @@ class AbstractTest(unittest.TestCase, abc.ABC):
         :param method_nums:
         :return:
         '''
-        if not method_nums:
-            return
-
-        method_prefix = self.get_method_name() + '_'
-        child_methods = [name for name in dir(self) if name.startswith(method_prefix)]
-        
-        if method_nums[0] is None:
-            method_nums = range(len(child_methods))
-        
-        method_prefixes = (f'{method_prefix}{i}' for i in method_nums)
-        methods = (getattr(self, actual_name) for expected_prefix in method_prefixes for actual_name in child_methods if actual_name.startswith(expected_prefix))
-
-        for method in methods:
+        for method in self.get_parameterized_methods_of_current_test(*method_nums):
             method()
--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from tests.abstractTest import AbstractTest
 from tests.categorierTest import CategorierTest
 from tests.finalNodeTest import FinalNodeTest
 from tests.glosbeTranslatorTest import GlosbeTranslatorTest
@@ -16,9 +17,20 @@ tests = [
 
 
 def main():
+    failure, errors, total = 0, 0, 0
     for test_class in tests:
         test = test_class()
         unittest.main(module=test, exit=False)
+
+        failure += test.failure
+        errors += test.errors
+        total += test.total
+
+    print()
+    print('#' * (2 * AbstractTest.half_sep_length))
+    print('Total test statistics:')
+    AbstractTest.print_statistics(failure, errors, total)
+
 
 
 if __name__ == '__main__':
